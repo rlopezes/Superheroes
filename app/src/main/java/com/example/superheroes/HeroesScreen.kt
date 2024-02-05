ee5b3ec845ed94c0526deb24d4649f69d69fc08d
rlopezes <rlopez.es@gmail.com>
2024-02-05 18:37:10 +0100

    Comentarios

--- app/src/main/java/com/example/superheroes/HeroesScreen.kt
+++ app/src/main/java/com/example/superheroes/HeroesScreen.kt
@@ -5,13 +5,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
@@ -21,7 +19,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,8 +31,9 @@ fun HeroesList(heroList: List<Hero>,
                contentPadding: PaddingValues = PaddingValues(0.dp),
                modifier: Modifier = Modifier
 ) {
-    LazyColumn(modifier = modifier,
-        contentPadding = contentPadding) {
+    LazyColumn(contentPadding = contentPadding,
+        modifier = modifier
+        ) {
         items(heroList) {
             HeroCard(
                 hero = it,
@@ -51,6 +49,7 @@ fun HeroCard(
     modifier: Modifier = Modifier
 ) {
     Card(
+        //SOMBRA, SIMULA RELIEVE
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp),
         modifier = modifier
     ) {
@@ -58,20 +57,22 @@ fun HeroCard(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp)
-                //??
+                //ALTURA MÍNIMA IGUAL A LA IMAGEN
                 .sizeIn(minHeight = 72.dp)
         ) {
             Column(
                 modifier = Modifier
                     .padding(end=16.dp)
+                    //TEXTOS OCUPAN EL ANCHO POSIBLE, EMPUJAN A LA IMAGEN
                     .weight(1f)
             ) {
                 Text(
-                    text = LocalContext.current.getString(hero.nameRes),
+                    //SE PODRÍA USAR LocalContext.current.getString
+                    text = stringResource(hero.nameRes),
                     style = MaterialTheme.typography.displaySmall
                 )
                 Text(
-                    text = LocalContext.current.getString(hero.descriptionRes),
+                    text = stringResource(hero.descriptionRes),
                     style = MaterialTheme.typography.bodyLarge
                 )
             }
@@ -82,7 +83,7 @@ fun HeroCard(
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .size(72.dp)
-                    .clip(RoundedCornerShape(8.dp))
+                    .clip(MaterialTheme.shapes.small)
             )
         }
     }
--- app/src/main/java/com/example/superheroes/MainActivity.kt
+++ app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.superheroes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -12,7 +11,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -41,12 +39,13 @@ class MainActivity : ComponentActivity() {
 fun SuperheroesApp(modifier: Modifier = Modifier) {
     Scaffold(
         topBar = {
-            HeroTopAppBar()
+            HeroTopAppBar(modifier)
         }
     ) {
         HeroesList(
             heroList = HeroesRepository.heroes,
-            contentPadding = it
+            contentPadding = it, //it = innerPadding
+            modifier = modifier
         )
     }
 }
